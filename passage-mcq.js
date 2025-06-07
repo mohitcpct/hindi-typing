2a4c04f1e263630ac455dc4595e3feff4af39fe4
mohitcpct <mohitcyclist@gmail.com>
2025-06-07 18:32:55 +0530

    Update passage-mcq.js

--- passage-mcq.js
+++ passage-mcq.js
@@ -179,8 +179,233 @@ demo2: {
   }
           ]
         }
+      },
+         demo3: {
+        label: "31th May 2025 - Shift 1",
+        hindi: {
+          passage: `म्यांमार के लोग जल को देवताओं का दिया हुआ आशीर्वाद मानते हैं। वहां जल को पवित्र माना जाता है है और यह सभी धार्मिक समारोहों का एक महत्वपूर्ण भाग होता है। वहाँ के लोग मित्रों और संबंधियों को उपहार में जल देते हैं क्योंकि ऐसा माना जाता है कि यह समृद्धि लाता है। वे अपनी गलियों और दरवाजों पर से गुजरने वाले अजनबियों और थके हुए यात्रियों को भी जल पिलाते हैं। लोग अपने घरों के प्रवेश द्वार पर या पैदल गामी मार्गों पर जल से भरे घड़े रखते हैं। वे थके हुए यात्रियों को सुकून प्रदान करने हेतु आसपास पेड़-पौधे भी लगा देते हैं। एक बर्मी प्रार्थना जल अर्पित करने के पीछे की नेक भावना को व्यक्त करती है: \n\n"वह जल के समान शीतल और फूलों समान कोमल हो।" \n\n म्यांमार के लोग नव वर्ष का उत्सव इस आशा के साथ मनाते हैं कि बारिश भरपूर होगी और समय पर आएगी। इस उत्सव को वहां थिंगयान' के रूप में जाना जाता है। यह उत्सव सामान्यतः अप्रैल महीने के मध्य में मनाया जाता है, जोकि वर्ष का सबसे गर्म और सबसे शुष्क महीना होता है। यह जल का त्यौहार है जिसमें नलों और कूपों से जल भर-भर कर लोग एक दूसरे पर जल फेंकते हैं। यह कुछ-कुछ भारत में मनाई जाने वाली होली त्योहार से मिलता जुलता त्योहार है। थिंगयान के अवसर पर म्यांमार में हर कोई भीगता है। इमारतों और मंदिरों को जल से भिगोया जाता है इस वजह से वहाँ सड़कें, छोटी नदियों की तरह दिखनी शुरू हो जाती हैं! लोग एक-दूसरे को नव वर्ष की बधाई देते हैं और प्रार्थना करते हैं कि नव वर्ष में अच्छी बारिश सभी के लिए समृद्धि लेकर आए। लोग ढोल बजाते हुए के बीच मंदिरों के जुलूस में शामिल होते हैं। वे मंदिरों में बौद्ध प्रतिमाओं को नहलाते हैं। बौद्ध यह विश्वास रखते हैं कि जल एक शोधक है। यह हमें हमारे बुरे विचारों और कर्मों से शुद्ध करता है। यह सभी पापों को धो देता है ताकि लोग नव वर्ष से एक स्वच्छ और निर्मल शुरुआत कर सकें। "`,
+          questions: [
+           {
+    "q": "म्यांमार के लोग जल को ______ मानते हैं।",
+    "options": [
+      "1. दुर्लभ",
+      "2. पवित्र",
+      "3. ठंडा",
+      "4. ताजा"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "उपहार में जल देने से निम्‍न में से क्या प्राप्‍त होता है?",
+    "options": [
+      "1. अच्छा स्वास्‍थ्‍य",
+      "2. ताज़गी",
+      "3. दीर्घायु",
+      "4. समृद्धि"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "थिंगयान का त्यौहार निम्‍न में से किसके लिए प्रार्थना करने हेतु मनाया जाता है?",
+    "options": [
+      "1. अच्छी फसल",
+      "2. यात्रियों की सुविधा",
+      "3. अच्छी बारिश",
+      "4. आत्मा की शुद्धता"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "गद्यांश के अनुसार म्यांमार के लोग जल को शोधक क्यों मानते हैं?",
+    "options": [
+      "1. क्योंकि इसे अजनबियों के साथ भी साझा किया जा सकता है।",
+      "2. क्योंकि इसका उपयोग इमारतों और मंदिरों को धोने के लिए किया जाता है।",
+      "3. क्योंकि यह उनकी प्रार्थनाओं और धार्मि क समारोहों का प्रमुख भाग है।",
+      "4. क्योंकि यह उनके बुरे विचारों और कर्मों को धो देता है।"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "गद्यांश के अनुसार निम्‍नलिखित में से कौन-सा कथन सही है?",
+    "options": [
+      "1. लोग अपने मित्रों को महंगे घड़े उपहार में देते हैं।",
+      "2. थिंगयान जल का त्यौहार है।",
+      "3. थिंगयान का त्यौहार मानसून के दौरान मनाया जाता है।",
+      "4. लोग शांति पूर्वक मंदिर के लिए शोभा यात्रा निकालते हैं।"
+    ],
+    "answer": 1
+  }
+          ]
+        },
+        english: {
+          passage: `Water is regarded as a blessing from the gods by the people of Myanmar. Water is considered to be sacred and is an important part of all religious ceremonies. People gift water to friends and relatives as it is believed to bring prosperity. They also share their water with strangers and weary travellers who may happen to pass by their doors. People keep jars full of water at the entrance of their homes or by the walkways. They also plant small green shoots close by to comfort the hearts of the tired travellers. A Burmese prayer expresses the noble spirit behind offering water: \n\n “May he be cool as water and fresh as flowers.” \n\n The people of Myanmar celebrate the New Year with the hope that the rains will be in plenty and will come on time. This festival is known as ‘Thingyan’. It is usually celebrated around the middle of April, the hottest and driest month of the year. It is a water festival when hose pipes and pumps are turned on and people throw water on each other. It is somewhat similar to the festival of Holi in India. On Thingyan, everyone and everything is wet in Myanmar. The buildings and temples are soaked in water and the streets begin to look like tiny rivers! People wish each other a happy New Year and pray that the New Year is as comforting as the water. People join the procession to the temples amidst the beating of drums. They wash the Buddhist images in the temples. The Buddhists believe that water is a purifier. It cleanses us of our evil thoughts and deeds. It washes away all sins so that the people can begin the New Year all clean and pure.” `,
+          questions: [
+            {
+    "q": "The people of Myanmar believe that water is:",
+    "options": [
+      "1. scarce",
+      "2. sacred",
+      "3. cool",
+      "4. fresh"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "A gift of water is believed to bring:",
+    "options": [
+      "1. good health",
+      "2. refreshment",
+      "3. longevity",
+      "4. prosperity"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "Thingyan is celebrated to pray for:",
+    "options": [
+      "1. a good harvest",
+      "2. the comfort of travellers",
+      "3. good rainfall",
+      "4. the soul’s purity"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "According to the passage, the people of Myanmar believe that water is a purifier because it:",
+    "options": [
+      "1. can be shared with strangers",
+      "2. is used to soak their buildings and temples",
+      "3. is a part of their prayers and religious ceremonies",
+      "4. cleanses them of their wicked thoughts and actions"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "Which of the following statements is true as per the passage?",
+    "options": [
+      "1. People gift expensive jars to friends.",
+      "2. Thingyan is a festival of water.",
+      "3. Thingyan is celebrated during the monsoons.",
+      "4. People carry out silent processions to the temples."
+    ],
+    "answer": 1
+  }
+          ]
+        }
+      },
+demo4: {
+        label: "31th May 2025 - Shift 2",
+        hindi: {
+          passage: `हाल ही में पर्यावरण के प्रति लोगों में जागरूकता पैदा करने के लिए दिल्ली के एक स्कूल द्वारा छह किलोमीटर दूरी की एक साइकिल रैली का आयोजन किया गया। दिल्ली के मुख्यमंत्री ने इस रैली को हरी झंडी दिखाकर इस कार्यक्रम का शुभारंभ किया। रैली में राजधानी के विभिन्न स्कूलों के हजारों प्रतिभागियों ने भाग लिया। साइकिल सवार लोग अपने साथ तख्तियां लिए हुए थे जिस पर लिखा था प्रदुषण मुक्त है हमारी साइकिल सवारी। अधिकांश बच्चे इस बात से उत्साहित थे कि उन्हें साइकिल की सवारी करने के लिए इतनी लंबी और खुली जगह उपलब्ध कराई गई। \n\n यह सत्य है कि साइकिल सवारी के अत्यधिक उपयोग से शहर के पर्यावरण पर लाभकारी प्रभाव होगा। लेकिन साइकिल चलाने के और भी कई लाभ हैं। यह उत्साहवर्धक है कि बच्चों को इन लाभों की संपूर्ण जानकारी है। रैली के एक प्रतिभागी के अनुसार, "साइकिल की सवारी स्वास्थ्यवर्धक है और यह शरीर को दुरुस्त रखती है।" एक अन्य बच्चे ने बताया कि साइकिल की सवारी, कार की सवारी से अधिक आनंददायक होती है। कुछ उत्साहित बच्चों ने मुख्यमंत्री से आग्रह किया की उनके लिए शहर की सड़कों पर एक अलग ट्रैक बनवाया जाए ताकि वे सुरक्षित रूप से साइकिल से अपने स्कूल जा सकें। \n\n रैली के उद्‌घाटन समारोह के अपने संक्षिप्त भाषण में मुख्यमंत्री ने कहा कि पर्यावरणीय प्रदूषण से लड़ना हर नागरिक का कर्तव्य है और इन बच्चों से प्रेरणा लेकर अधिक से अधिक लोगों को, कम से कम छोटी दूरी के लिए ही सही लेकिन साइकिल का उपयोग निश्चित रूप से करना चाहिए। \n\n इस अवसर पर कई अन्य वक्ताओं ने बच्चों के साथ अपने विचार प्रकट किये। 'प्रोजेक्ट टाइगर' पर एक क्विज का भी आयोजन किया गया था। स्वस्थ शरीर के लिए साइकिल सवारी के महत्व पर केन्द्रित इकोफ्रेंड (ECOFRIEND) नामक पत्रिका के विशेष संस्करण का भी विमोचन किया गया।"`,
+          questions: [
+            {
+    "q": "दिल्ली में साइकिल रैली का आयोजन किसलिए किया गया था ?",
+    "options": [
+      "1. साइकिलों की बिक्री बढ़ाने के लिए",
+      "2. अच्छे स्वास्‍थ्‍य को बढ़ावा देने के लिए",
+      "3. पर्यावरण जागकता पैदा करने के लिए",
+      "4. साइकिल चलाना अनिवार्य बनाने के लिए"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "प्रदूषण से लड़ना किसकी ज़िम्मेदारी है?",
+    "options": [
+      "1. सिर्फ सरकार की",
+      "2. हर नागरिक की",
+      "3. सभी स्कूलों की",
+      "4. सभी बच्चों की"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "बच्चों की क्विज का विषय (थीम) क्या था ?",
+    "options": [
+      "1. साइकलिंग का महत्‍व",
+      "2. प्रदूषण मुक्‍त पर्यावरण",
+      "3. शारीरिक स्‍वस्‍थता",
+      "4. बाघ संरक्षण"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "तख्तियों पर प्रदर्शित आदर्श वाक्‍य (मोटो ) क्या था ?",
+    "options": [
+      "1. राइडिंग मजेदार है",
+      "2. पर्यावरण बचाओ",
+      "3. प्रदूषण से लड़ो",
+      "4. प्रदुषण मुक्‍त है हमारी साइकिल सवारी"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "बच्चों ने अलग ट्रैक की मांग का प्रस्ताव क्यों रखा ?",
+    "options": [
+      "1. मित्रों के साथ सवारी का आनंद लेने के लिए",
+      "2. अपनी सुरक्षा सुनिश्चित करने के लिए",
+      "3. यातायात सिग्नल से बचने के लिए",
+      "4. समय पर स्कूल पहुंचने के लिए"
+    ],
+    "answer": 1
+  }
+          ]
+        },
+        english: {
+          passage: `Recently, a six-km long bicycle rally was organised by a school in Delhi to promote environmental awareness among people. The rally was kicked off by the Chief Minister of Delhi. It attracted over a thousand participants from schools all over the capital. The cyclists carried placards with the motto ‘Pollution free vehicle is my cycle.’ Most of the children felt thrilled by the experience of riding a bicycle in open spaces for such a long distance. \n\nIt is certainly true that an increase in the use of bicycles would have a beneficial effect on the environment of the city. But cycling has other benefits too. It was encouraging to see that the children were fully aware of these benefits. According to one of the participants, “Riding a bicycle leads to sound health and keeps the body fit.” Another child said that riding a cycle was more fun than sitting in a car. Some excited children requested the Chief Minister to provide separate cycling tracks on city roads so that they could ride safely to their schools. \n\n In a short speech while inaugurating the rally, the Chief Minister said that it is every citizen’s responsibility to fight environmental pollution and taking a cue from these children, more and more people should turn to bicycles, at least for short distances. \n\n On this occasion several speakers shared their views with the children. There was a quiz on ‘Project Tiger’. A special issue of the magazine ECOFRIEND conveying the importance of cycling for physical fitness was also released.” `,
+          questions: [
+            {
+    "q": "The cycle rally was organised in Delhi mainly to:",
+    "options": [
+      "1. increase the sale of bicycles",
+      "2. promote good health",
+      "3. create environmental awareness",
+      "4. make cycling compulsory"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Fighting pollution is the responsibility of:",
+    "options": [
+      "1. only the government",
+      "2. every citizen",
+      "3. all schools",
+      "4. all children"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "The theme of the children’s quiz was:",
+    "options": [
+      "1. the importance of cycling",
+      "2. pollution-free environment",
+      "3. physical fitness",
+      "4. tiger conservation"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "What was the motto displayed on the placards?",
+    "options": [
+      "1. Riding is fun",
+      "2. Save the environment",
+      "3. Fight pollution",
+      "4. Pollution free vehicle is my cycle"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "Why did children ask for separate cycle tracks?",
+    "options": [
+      "1. To enjoy riding with friends",
+      "2. To ensure their safety",
+      "3. To avoid traffic signals",
+      "4. To reach school on time"
+    ],
+    "answer": 1
+  }
+          ]
+        }
       }
-
     };
 
     window.onload = function () {
