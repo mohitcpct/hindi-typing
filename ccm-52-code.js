3d5ed674f1a7658170834f23a1354139e330e286
mohitcpct <mohitcyclist@gmail.com>
2025-06-19 10:19:37 +0530

    Update ccm-52-code.js

--- ccm-52-code.js
+++ ccm-52-code.js
@@ -9369,7 +9369,6 @@
   {
     "q": "In MS-Excel 2019, given picture is an example of a ________________. <br><img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEfTqDQD7Vxi3iuuZo4b13BwmGkGtAZR6WtarLuwEfHpO7FvTiKTskLQsEKbfFMf4sgbtwDA7FXqeWG1px1D435-TuhrcnT10S8gj61XztC9vfC8DiXnfVG9XnuB-g3B8KrvphS84GX6ieavd6AyDeZi2_zzBhKa5SHjb6SbLaR8LU_r-CXaK_SGUihYQl/s320/34%20fd.png' style='max-width:100%;height:auto;border:2px solid #ccc;border-radius:6px;' />",
     "options": [
-   
       "1. Pie chart",
       "2. Bar chart",
       "3. Line graph",
@@ -9380,7 +9379,6 @@
   {
     "q": "MS-Excel 2019 में, दी गई तस्वीर ________________ का एक उदाहरण है। <br><img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEfTqDQD7Vxi3iuuZo4b13BwmGkGtAZR6WtarLuwEfHpO7FvTiKTskLQsEKbfFMf4sgbtwDA7FXqeWG1px1D435-TuhrcnT10S8gj61XztC9vfC8DiXnfVG9XnuB-g3B8KrvphS84GX6ieavd6AyDeZi2_zzBhKa5SHjb6SbLaR8LU_r-CXaK_SGUihYQl/s320/34%20fd.png' style='max-width:100%;height:auto;border:2px solid #ccc;border-radius:6px;' />",
      "options": [
-     
       "1. Pie chart",
       "2. Bar chart",
       "3. Line graph",
