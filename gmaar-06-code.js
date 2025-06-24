779e5b8e5398b778ff712ebb78262dac78cb84aa
mohitcpct <mohitcyclist@gmail.com>
2025-06-24 07:17:12 +0530

    Add files via upload

---
+++ gmaar-06-code.js
@@ -0,0 +1,129 @@
+ const questionBank = {
+  "30th May 2025 - Shift1": [
+ {
+    "q": "Tennis' is related to 'Court' in the same way as 'Boxing' is related to '_______'.",
+    "options": [
+      "1. Pool",
+      "2. Ring",
+      "3. Arena",
+      "4. Course"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "टेनिस' का 'कोर्ट' से वही संबंध है जो 'बॉक्सिंग' का '________' से संबंध है।",
+    "options": [
+      "1. पूल",
+      "2. रिंग",
+      "3. एरिना",
+      "4. कोर्स"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "Each of the given letters has been assigned a unique number. Select the combination of numbers such that the corresponding letters when arranged accordingly will form a meaningful word. <br><br>E M I H T R<br>1 2 3 4 5 6",
+    "options": [
+      "1. 1, 2, 3, 4, 5, 6",
+      "2. 4, 1, 6, 2, 3, 5",
+      "3. 5, 1, 6, 4, 3, 2",
+      "4. 6, 1, 2, 3, 5, 4"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "दिए गए प्रत्येक अक्षर को एक विशिष्‍ट संख्या निर्दिष्‍ट की गई है। संख्याओं के संयोजन का चयन इस प्रकार करें कि संगत अक्षरों को तदनुसार व्‍यवस्थित करने पर एक अर्थपूर्ण शब्‍द बने।<br><br>E M I H T R<br>1 2 3 4 5 6",
+    "options": [
+      "1. 1, 2, 3, 4, 5, 6",
+      "2. 4, 1, 6, 2, 3, 5",
+      "3. 5, 1, 6, 4, 3, 2",
+      "4. 6, 1, 2, 3, 5, 4"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "Select the word from the options that is similar to the given set of words in a certain way and hence belongs to the same group. <br><br>Peat : Lignite : Bituminous",
+    "options": [
+      "1. Granite",
+      "2. Basalt",
+      "3. Anthracite",
+      "4. Coke"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "विकल्पों में से वह शब्‍द चुनिए जो किसी निश्चित तरीके से दिए गए शब्दों के समूह के समान है और इसलिए उसी समूह से संबंधित है। <br><br> पीट : लिग्नाइट : बिटुमिनस",
+    "options": [
+      "1. ग्रेनाइट",
+      "2. बेसा",
+      "3. एंथ्रेसाइट",
+      "4. कोक"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Select the most appropriate description about the given set of words. <br><br>Wheat : Barley : Rice",
+    "options": [
+      "1. Food",
+      "2. Agriculture",
+      "3. Grams",
+      "4. Cereals"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "दिए गए शब्दों के समूह के बारे में सबसे उपयुक्‍त विवरण का चयन करें। <br><br> गेहूं: जौ : चावल",
+    "options": [
+      "1. भोजन",
+      "2. कृषि",
+      "3. ग्राम",
+      "4. अनाज"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "Select the option that is true regarding the following two statements labelled Assertion (A) and Reason (R). <br><br>Assertion (A): The pouring of kerosene oil on stagnant pools helps to eradicate malaria. <br> Reason (R): Kerosene oil is poisonous for mosquitoes.",
+    "options": [
+      "1. Both A and R are true and R is the correct explanation of A.",
+      "2. Both A and R are true but R is not the correct explanation of A.",
+      "3. A is true but R is false.",
+      "4. A is false but R is true."
+    ],
+    "answer": 2
+  },
+  {
+    "q": "उस विकल्‍प का चयन कीजिए जो अभिकथन (A) और कारण (R) नामक निम्‍नलिखित दो कथनों के संबंध में सत्‍य है। <br><br>अभिकथन (A): ठहरे हुए पानी पर केरोसिन तेल डालना मलेरिया को खत्‍म करने में मदद करता है। <br>कारण (R): केरोसिन तेल मच्‍छरों के लिए जहरीला होता है।",
+    "options": [
+      "1. A और R दोनों सत्‍य हैं और R, A का सही स्‍पष्टीकरण हैं।",
+      "2. A और R दोनों सत्‍य हैं परंतु R, A का सही स्‍पष्टीकरण नहीं हैं।",
+      "3. A सत्‍य है परंतु R असत्‍य है।",
+      "4. A असत्‍य है परंतु R सत्‍य है।"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Select the option which satisfies the same conditions of placement of the dot as in figure shown here. <br> <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg93MxsQkbO6tqrJjGckYq2M0_0Jdk9vee8xsyHAnTDdygPJPoe4vvOSeFyN5JlBVonoVt3761s4Vynddsz3LVwt15VcisPpbNc0be3z6P4HShhcGdDUAJLsOkXVqIa7xfTYwFKCorPUPI2spq8WbHxCt1jcX3MT0ZNqed6_dYM14eDIxyFKp-14PlWMhVg/s1600/1%20bb.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgoP3dq0WtO8bE1EHrQJBKNfIQo8tSoPxhPG0zrFPpFvBlt4XWF4v8OnKY4o3JWlgmn9NWuCHPofzrzOlugDbOihKV1Ob9FISq8ZFuLqqaIsDgZiCK7YDPp4cukDT22oTqaazSAl4ntNEFOz9y6Ncor9AatUXiyrQJOLz7grHBHSNd5mYtzFlwI10zjM0AB/s1600/2%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiusUic0xXDwuC6LebG0b6BSxzCdNSYMk-mCuD0wSnccdzjF21BuyKgBQUTH8Tj2IC01lkKmD15FwJu6rK1vbOA3QE6N0Pa6uP8wB2kiSjwbpjKXSoLFVA7PGz10nkfbzDJVPQ9yuW6XHtnEIpoemJfflQE068TEZMZw3I_9MedfU94sZKun_GE4jOASBcP/s1600/3%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg34tkCOg0vBBZBDuV1SDFCIZv5hmxHt6kug0chYUsc1kFEMMIhJQOi39II3Pp4qUPo-4W3gj2LfBEs-I9OIJLzge5Mv5Lzuf_M60YrArYxDTiW5-0Rx7S1ydgp2qjIOrn2gII2Em-0fM_J2v-k5N3fCGzoMlSaU8OXtAEXG5vzAorb5GDSAHVl0cKbPyEF/s1600/4%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEirrbUgfo9JBC7OryQnIkqIDkswWltSLZeUtgxY383S7Fp-rwLQvjPPJsVLIexoICKAl80TP2RYFrEu8t9fLZA_mHNtTkCcDhKNM_fHLajz0Vw5U2QJJuSYSf8Rrv-0MGn5G0GEKPNun9uttk8BSoVoJ59lUQn6Px-Mb3expZiCaKkG-0arVzyCKgGyDkc9/s1600/5%20bb.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "उस विकल्‍प का चयन करें जो नीचे प्रदर्शित चित्र के अनुसार बिंदुओं के स्थान-निर्धारण की समान शर्तों को पूर्ण करता है। <br> <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg93MxsQkbO6tqrJjGckYq2M0_0Jdk9vee8xsyHAnTDdygPJPoe4vvOSeFyN5JlBVonoVt3761s4Vynddsz3LVwt15VcisPpbNc0be3z6P4HShhcGdDUAJLsOkXVqIa7xfTYwFKCorPUPI2spq8WbHxCt1jcX3MT0ZNqed6_dYM14eDIxyFKp-14PlWMhVg/s1600/1%20bb.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgoP3dq0WtO8bE1EHrQJBKNfIQo8tSoPxhPG0zrFPpFvBlt4XWF4v8OnKY4o3JWlgmn9NWuCHPofzrzOlugDbOihKV1Ob9FISq8ZFuLqqaIsDgZiCK7YDPp4cukDT22oTqaazSAl4ntNEFOz9y6Ncor9AatUXiyrQJOLz7grHBHSNd5mYtzFlwI10zjM0AB/s1600/2%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiusUic0xXDwuC6LebG0b6BSxzCdNSYMk-mCuD0wSnccdzjF21BuyKgBQUTH8Tj2IC01lkKmD15FwJu6rK1vbOA3QE6N0Pa6uP8wB2kiSjwbpjKXSoLFVA7PGz10nkfbzDJVPQ9yuW6XHtnEIpoemJfflQE068TEZMZw3I_9MedfU94sZKun_GE4jOASBcP/s1600/3%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg34tkCOg0vBBZBDuV1SDFCIZv5hmxHt6kug0chYUsc1kFEMMIhJQOi39II3Pp4qUPo-4W3gj2LfBEs-I9OIJLzge5Mv5Lzuf_M60YrArYxDTiW5-0Rx7S1ydgp2qjIOrn2gII2Em-0fM_J2v-k5N3fCGzoMlSaU8OXtAEXG5vzAorb5GDSAHVl0cKbPyEF/s1600/4%20bb.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEirrbUgfo9JBC7OryQnIkqIDkswWltSLZeUtgxY383S7Fp-rwLQvjPPJsVLIexoICKAl80TP2RYFrEu8t9fLZA_mHNtTkCcDhKNM_fHLajz0Vw5U2QJJuSYSf8Rrv-0MGn5G0GEKPNun9uttk8BSoVoJ59lUQn6Px-Mb3expZiCaKkG-0arVzyCKgGyDkc9/s1600/5%20bb.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 1
+  }
+]
+
+
+        
+        };
+    
+    
