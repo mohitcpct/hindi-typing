f241a86a1a431d45404ac787e5ad739e15c3eb6e
mohitcpct <mohitcyclist@gmail.com>
2025-06-24 08:14:30 +0530

    Update gmaar-06-code.js

--- gmaar-06-code.js
+++ gmaar-06-code.js
@@ -120,6 +120,128 @@
     ],
     "answer": 1
   }
+],
+  "30th May 2025 - Shift2": [
+  {
+    "q": "Wax' is related to 'Grease' in the same way as 'Milk' is related to '________'.",
+    "options": [
+      "1. Drink",
+      "2. Ghee",
+      "3. Curd",
+      "4. Protein"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "मोम' का 'ग्रीस' से वही संबंध है जो संबंध 'दूध' का '________' से है।",
+    "options": [
+      "1. पेय",
+      "2. घी",
+      "3. दही",
+      "4. प्रोटीन"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Each of the given letters has been assigned a unique number. Select the combination of numbers such that the corresponding letters when arranged accordingly will form a meaningful word. <br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEguJ3L5H9j3slAfAk5KXD_1VhCj1IfqQZZF-J81qsPcf01cesNjj9Tq3-mezsPZ_xYgwMx_1L-VHQlAxwFhhAaBhzm7GOYnENtNkXGmRW8UVg02V7zFzppGXM5h2BFJsFgPEgMp-IyaU0HD7r1ID184c-uECeZsRIEK1AF4IrVcFbUP09NOybDUcpIE8xZ4/s1600/1%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 5102674938",
+      "2. 5180427639",
+      "3. 5617283094",
+      "4. 7450823796"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "दिए गए प्रत्येक अक्षर को एक विशिष्‍ट संख्या दी गई है। संख्याओं का ऐसा संयोजन चुनें जिससे संगत अक्षरों को तदनुसार व्‍यवस्थित करने पर एक सार्थक शब्‍द बने।  <br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEguJ3L5H9j3slAfAk5KXD_1VhCj1IfqQZZF-J81qsPcf01cesNjj9Tq3-mezsPZ_xYgwMx_1L-VHQlAxwFhhAaBhzm7GOYnENtNkXGmRW8UVg02V7zFzppGXM5h2BFJsFgPEgMp-IyaU0HD7r1ID184c-uECeZsRIEK1AF4IrVcFbUP09NOybDUcpIE8xZ4/s1600/1%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 5102674938",
+      "2. 5180427639",
+      "3. 5617283094",
+      "4. 7450823796"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Select the option that is true regarding the following two statements labelled Assertion (A) and Reason (R). <br><br>Assertion (A): Plaster of Paris is used by doctors for setting fractured bones. <br>Reason (R): When Plaster of Paris is mixed with water and applied around the fractured limbs, it sets into a hard mass.",
+    "options": [
+      "1. Both A and R are true and R is the correct explanation of A.",
+      "2. Both A and R are true but R is not the correct explanation of A.",
+      "3. A is true but R is false.",
+      "4. A is false but R is true."
+    ],
+    "answer": 0
+  },
+  {
+    "q": "नीचे दिए गए प्रश्‍न में, अभिकथन (A) और कारण (R) के रूप में चिह्नित दो कथन दिए गए हैं। इसके अनुसार सही विकल्‍प का चयन कीजिए। <br><br>अभिकथन (A): फ्रैक्‍चर हड्डियों को सेट करने के लिए डॉक्‍टरों द्वारा प्लास्‍टर ऑफ़ पेरिस का उपयोग किया जाता है। <br>कारण (R): जब प्लाटर ऑफ़ पेरिस को पानी के साथ मिलाया जाता है और फ्रैक्‍चर हुए अंगों के चारों ओर लगाया जाता है, तो यह एक कठोर पदार्थ के रूप में जम जाता है।",
+    "options": [
+      "1. A और R दोनों सत्‍य हैं और R, A का सही स्‍पष्टीकरण है।",
+      "2. A और R दोनों सत्‍य हैं परंतु R, A का सही स्‍पष्टीकरण नहीं है।",
+      "3. A सत्‍य है परंतु R असत्‍य है।",
+      "4. A असत्‍य है परंतु R सत्‍य है।"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "Four words have been given, out of which three are alike in some manner and one is different. Select the word that is different. Options :",
+    "options": [
+      "1. Moth",
+      "2. Bee",
+      "3. Lizard",
+      "4. Aphid"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "विकल्पों में चार श दिए गए हैं जिनमें से तीन किसी प्रकार से संगत हैं, जबकि कोई एक उनसे असंगत है। असंगत शब्‍द का चयन कीजिए।",
+    "options": [
+      "1. पतंगा",
+      "2. मधुमक्खी",
+      "3. छिपकली",
+      "4. ऐफिड"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "Select the option that satisfies the same conditios of the placement of dots as shown in the given figure. <br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7d6-jN2yXNmhrr5ZqqiS9YnSSCTl79YJ1cp7QcgXFj5lonzj2xRoPfQ50wSWDRXi8TC24I1cBD7L7i994KlCHWAdZ5eDvivoeVZLb3Dwcw5TOW7I9NQq1kYQxbZOQ2AQagcRanCBlbSnxaAjqIprq_jq3ClpEefvxsruB7LdP0FK2ehWRDnKoKfxt-_01/s1600/2%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj2hk92V_ALns-JojoDFchb1mCWWjAdtT1uPZ5taPiDz6kQ2nHUwB6FU9XepU2tzc8ee78ysEN-lAV2DUh_qPGSntNTQsuNIHkE8X1cSNfrb1Fip_R9fPMmgLJpI4g4J-QeOFzDeWzM1q1mZRMDXsYetc57rgPUwUH5J343X_i6D1CAAsOJs8Jb4-sKfpx8/s1600/3%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhhDJ7OQpmS-NHoY0KmubSiLnmKD5YSTfhYJdSvT0of3JnK1C3FOWJarNrtgJPYPBwFvX2GOrcM73fSKOvvpIewe6-km_mQ92V7IWvft4tquMop52y4f0EKvAGAZw5cy472nCtyHn2xKyfNCfDDjZO7ElTX9UKnKXKHPXApr96b2INyG0rYhapaGIS_V5wT/s1600/4%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKTqqavVqV3mgoa1Xzc3KjITxXTmIhps7kUYxYysgT8JfM91VY0wc0zO8SsBjPWoEfSJqDpbw_CvlFfNDCrGCl45BjnlRtEZHaRVRr3KmrQRVp1pN9LhWmqyg5FzI3oAGq6sRhrfqygcrtHOaznHyw3qcRoMu_9Lc35CXBh5_DRY9iDgi-wRpzgEwNhiM3/s1600/5%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhiaXpafiOeNc3_Pa5QZWD8xoz6_Ulvcl6_FqwY13tpVQJhgj7gcVG7UxbYXMCnsoN6434KlRDbRYLZ5YzFjxkJtOsxZfVmta28pf_jyi9FiukHfJekK0J1qte2O2QeHaNRUfqUA4wostc23GXuj4MpoSQON9YEJCLOD2wYXzzBv2sBXdFlUUOivinZJHxl/s1600/6%20bbf.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "नीचे दर्शाये गए चित्र में बिंदुओं के स्‍थान नियोजन को संतुष्‍ट करने वाली स्थिति से संबंधित चित्र का चयन विकल्‍पों में से कीजिए। <br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7d6-jN2yXNmhrr5ZqqiS9YnSSCTl79YJ1cp7QcgXFj5lonzj2xRoPfQ50wSWDRXi8TC24I1cBD7L7i994KlCHWAdZ5eDvivoeVZLb3Dwcw5TOW7I9NQq1kYQxbZOQ2AQagcRanCBlbSnxaAjqIprq_jq3ClpEefvxsruB7LdP0FK2ehWRDnKoKfxt-_01/s1600/2%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj2hk92V_ALns-JojoDFchb1mCWWjAdtT1uPZ5taPiDz6kQ2nHUwB6FU9XepU2tzc8ee78ysEN-lAV2DUh_qPGSntNTQsuNIHkE8X1cSNfrb1Fip_R9fPMmgLJpI4g4J-QeOFzDeWzM1q1mZRMDXsYetc57rgPUwUH5J343X_i6D1CAAsOJs8Jb4-sKfpx8/s1600/3%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhhDJ7OQpmS-NHoY0KmubSiLnmKD5YSTfhYJdSvT0of3JnK1C3FOWJarNrtgJPYPBwFvX2GOrcM73fSKOvvpIewe6-km_mQ92V7IWvft4tquMop52y4f0EKvAGAZw5cy472nCtyHn2xKyfNCfDDjZO7ElTX9UKnKXKHPXApr96b2INyG0rYhapaGIS_V5wT/s1600/4%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKTqqavVqV3mgoa1Xzc3KjITxXTmIhps7kUYxYysgT8JfM91VY0wc0zO8SsBjPWoEfSJqDpbw_CvlFfNDCrGCl45BjnlRtEZHaRVRr3KmrQRVp1pN9LhWmqyg5FzI3oAGq6sRhrfqygcrtHOaznHyw3qcRoMu_9Lc35CXBh5_DRY9iDgi-wRpzgEwNhiM3/s1600/5%20bbf.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhiaXpafiOeNc3_Pa5QZWD8xoz6_Ulvcl6_FqwY13tpVQJhgj7gcVG7UxbYXMCnsoN6434KlRDbRYLZ5YzFjxkJtOsxZfVmta28pf_jyi9FiukHfJekK0J1qte2O2QeHaNRUfqUA4wostc23GXuj4MpoSQON9YEJCLOD2wYXzzBv2sBXdFlUUOivinZJHxl/s1600/6%20bbf.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "If in a certain code, the coding is done as follows: <br><br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnwkWWEpj5rsGO6lEa94OWlFGk37CQL1DlpG0Jq4bjEDC06DNYhC7UTX6GCGFSnVQOeMi5dXNfHdTXLSQMCruoYBQKGLJ2Uum5Crw-xvUIt6HzI0n51DjlCQRGk3ic5xgBKHEswdyHnpC3I5N43U2Qt8eNWQYZgQ7GvFXFN7FdlGPHawjF4Bhg-Js9qT7O/s1600/7%20bbf.png\" style=\"max-width:100%;height:auto;\">    <br><br>then how will PRFLKHV be coded?",
+    "options": [
+      "1. 4273658",
+      "2. 4273685",
+      "3. 4237865",
+      "4. 4237658"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "एक निश्चित कोड के लिए कोडिंग निम्नानुसार की गई है:  <br><br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnwkWWEpj5rsGO6lEa94OWlFGk37CQL1DlpG0Jq4bjEDC06DNYhC7UTX6GCGFSnVQOeMi5dXNfHdTXLSQMCruoYBQKGLJ2Uum5Crw-xvUIt6HzI0n51DjlCQRGk3ic5xgBKHEswdyHnpC3I5N43U2Qt8eNWQYZgQ7GvFXFN7FdlGPHawjF4Bhg-Js9qT7O/s1600/7%20bbf.png\" style=\"max-width:100%;height:auto;\">    <br><br>इस कोडिंग संरचना में PRFLKHV का कोड क्या होगा?",
+    "options": [
+      "1. 4273658",
+      "2. 4273685",
+      "3. 4237865",
+      "4. 4237658"
+    ],
+    "answer": 0
+  }
 ]
 
 
