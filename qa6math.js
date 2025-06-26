de197b0d82892beca409d66238fbd37aec0386d2
mohitcpct <mohitcyclist@gmail.com>
2025-06-26 09:44:21 +0530

    Add files via upload

---
+++ qa6math.js
@@ -0,0 +1,246 @@
+ const questionBank = {
+  "30th May 2025 - Shift1": [
+  {
+    "q": "There are some parrots and some tigers in a forest. If the total number of animal heads in the forest is 858 and the total number of animal legs is 1,846, what is the number of parrots in the forest?",
+    "options": [
+      "1. 845",
+      "2. 833",
+      "3. 800",
+      "4. 793"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "एक जंगल में कुछ तोते और कुछ बाघ हैं। यदि जंगल में कुल जीवों के सिरों की संख्या 858 है और पैरों की संख्या 1,846 है, तो जंगल में तोतों की संख्या कितनी है?",
+    "options": [
+      "1. 845",
+      "2. 833",
+      "3. 800",
+      "4. 793"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "What appropriate value should come in place of the question mark (?) in the given equation? <br>(Calculate to the nearest integer value) <br><img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKP_T1Co0bvLaNzx-r2l3sU9AB0ITnjGMCoRJrPIyKPmmivWdyGrMOvM90KWjtJoyq5EzpcXzg2-kehNNbmgmg8Df2CUTa-7z_zPktymg6dblxByUPKr7ShrHeWEfwbYI4kULm8lgDF3R35XnqBRgONwoBFf86ueQfMbqjNprDdqBcWEGgxvRreuJwusup/s1600/125g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 4445",
+      "2. 4545",
+      "3. 4495",
+      "4. 4595"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "दिए गए समीकरण में चिह्न (?) के स्थान पर क्या उचित मान आना चाहिए? <br>(निकटतम पूर्णांक मान की गणना करें) <br> <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKP_T1Co0bvLaNzx-r2l3sU9AB0ITnjGMCoRJrPIyKPmmivWdyGrMOvM90KWjtJoyq5EzpcXzg2-kehNNbmgmg8Df2CUTa-7z_zPktymg6dblxByUPKr7ShrHeWEfwbYI4kULm8lgDF3R35XnqBRgONwoBFf86ueQfMbqjNprDdqBcWEGgxvRreuJwusup/s1600/125g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 4445",
+      "2. 4545",
+      "3. 4495",
+      "4. 4595"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "What will come in place of the question mark (?) in the given number series? <br><br>71 78 92 120 ? 288",
+    "options": [
+      "1. 160",
+      "2. 176",
+      "3. 199",
+      "4. 208"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "श्रृंखला में प्रश्‍नवाचक चिह्न (?) के स्थान पर कौन-सी संख्या आएगी? <br><br>71 78 92 120 ? 288",
+    "options": [
+      "1. 160",
+      "2. 176",
+      "3. 199",
+      "4. 208"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "What will be the average of the following set of scores? (Round off to the nearest integer) <br><br>46, 54, 62, 68, 56, 29, 58",
+    "options": [
+      "1. 45",
+      "2. 59",
+      "3. 62",
+      "4. 53"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "अंकों के निम्‍न समुच्‍चय का औसत क्या होगा? (निकटतम पूर्णांक तक) <br><br>46, 54, 62, 68, 56, 29, 58",
+    "options": [
+      "1. 45",
+      "2. 59",
+      "3. 62",
+      "4. 53"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "A and B undertook to complete a work for ₹7,200. A could do it alone in 5 days, and B alone in 8 days. With the help of C and D, they finished it in 3 days. If the work done by C is twice that of D, the share of A exceeded the share of B by:",
+    "options": [
+      "1. ₹1,480",
+      "2. ₹1,620",
+      "3. ₹1,740",
+      "4. ₹1,860"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "A और B ने ₹7,200 में एक काम पूरा करने का अनुबंध किया। A अकेले उस काम को 5 दिनों में कर सकता है, और B अकेले 8 दिनों में। C और D की मदद से उन्होंने वह काम 3 दिनों में पूरा कर लिया। यदि C द्वारा किया गया कार्य D के कार्य का दुगना है, तो A का हिस्सा B के हिस्से से कितना अधिक था?",
+    "options": [
+      "1. ₹1,480",
+      "2. ₹1,620",
+      "3. ₹1,740",
+      "4. ₹1,860"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "A person borrowed some money every year at 10% compound interest. If he pays ₹25,487 at the end of 3 years, how much money had he borrowed every year?",
+    "options": [
+      "1. ₹7,000",
+      "2. ₹7,200",
+      "3. ₹7,500",
+      "4. ₹8,000"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "एक व्‍यक्ति 10% चक्रवृद्धि ब्याज की दर से प्रतिवर्ष कुछ धनराशि उधार लेता है। यदि वह 3 वर्ष के अंत में ₹25,487 का भुगतान करता है, तो उसने प्रति वर्ष कितना धन उधार लिया था?",
+    "options": [
+      "1. ₹7,000",
+      "2. ₹7,200",
+      "3. ₹7,500",
+      "4. ₹8,000"
+    ],
+    "answer": 0
+  }
+],
+
+"30th May 2025 - Shift2": [
+  {
+    "q": "The ratio of the students in schools A, B and C is 5 : 4 : 7 respectively. If the number of students in the school are increased by 20%, 25% and 20%, respectively. What would be the new ratio of the students in schools A, B and C?",
+    "options": [
+      "1. 5 : 5 : 7",
+      "2. 30 : 25 : 42",
+      "3. 30 : 20 : 49",
+      "4. Cannot be determined"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "स्कूल A, B और C में छात्रों का अनुपात क्रमशः 5 : 4 : 7 है। यदि स्कूलों में छात्रों की संख्या क्रमशः 20%, 25% और 20% बढ़ जाती है तो स्कूल A, B और C में छात्रों का नया अनुपात क्या होगा?",
+    "options": [
+      "1. 5 : 5 : 7",
+      "2. 30 : 25 : 42",
+      "3. 30 : 20 : 49",
+      "4. निर्धारित नहीं किया जा सकता है"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "Two equations numbered I and II are given. You have to solve both the equations and select the answer from the given options. <br><br>I. 88x² - 19x + 1 = 0<br>II. 132y² - 23y + 1 = 0",
+    "options": [
+      "1. x ≥ y",
+      "2. x > y",
+      "3. Relationship between x and y cannot be established",
+      "4. x < y"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "I और II क्रमांकित दो समीकरण दिए गए हैं। आपको दोनों समीकरणों को हल करना है और उत्‍तर अंकित करना है।<br><br>I. 88x² - 19x + 1 = 0<br>II. 132y² - 23y + 1 = 0",
+    "options": [
+      "1. x ≥ y",
+      "2. x > y",
+      "3. x और y के बीच संबंध स्‍थापित नहीं किया जा सकता।",
+      "4. x < y"
+    ],
+    "answer": 0
+  },
+  {
+    "q": "Four-seventh of a number is equal to 40% of another number. What is the ratio between the first number and the second number, respectively?",
+    "options": [
+      "1. 5 : 4",
+      "2. 4 : 5",
+      "3. 10 : 7",
+      "4. 7 : 10"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "एक संख्या का 4/7 वां भाग दूसरी संख्या के 40% के बराबर है। पहली संख्या और दूसरी संख्या के बीच क्रमशः अनुपात क्या है?",
+    "options": [
+      "1. 5 : 4",
+      "2. 4 : 5",
+      "3. 10 : 7",
+      "4. 7 : 10"
+    ],
+    "answer": 3
+  },
+  {
+    "q": "<img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgkyYpFpS5xm05h9dIudNPMH1s5fjGMtbdA_5UxJBqpsZq1uHiMFn4nuhLsl7uDvp2qkMclRwnYKg9XkWGJZjE6Sbm3PBPzQXM75-QUFBoK9Rf0di3hV4O7y9rCGok464GqSXK4l5sGGHgDejRKHnVXQdw6T-DaL7Da3H_Ibvm2ExbXLnAYKtrmSFql_IXm/s320/126g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 0",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhafgVexCAE6yHUJFM_Te-a9DR1PCSrWHRvfJeN1cL1Gfi2cAffhRl9k88ui4yXTHQx4UcFTRMHcjO2mhNRyprLYVFQHbaS3j2NVyy9vyQZmaasNgvCQAigGIYKxwHGuMaEhzPIbHKziidz53corfccvGeBaKQBmWPQXUEXK27uYQIbggBEH0zOhxP_w1ZK/s1600/128g.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjbXav5QMOfsbCnB1vs1K6nPjZzM4nBidPu0ogk9iF0Fui9Z0SBycrnWFH2nUGv4xKfBxOI-iN70x6tTbt97YLnBNi_KyATfNH8tZzbk1f3vDwyGrit3mCh-k-W3n2T8Taqz-Q1ZAE3A8CPSlN-uq-h67bDENU-WGXVwGAhTcjo8cU1tZkHiBHFOabyLXiR/s1600/129g.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgmn40NVkbAd-ENbta2-KLtX0z6JLWhEHNPEOHjsOIsZoiKpRdwqtQpC0QcecivbRu7sJSiGpZzf6PhxT_0N-D9X7t3-GT_qMZ2YioU-RN9vHns0Rtyarmc_IMCgKSd_8J93ajXMf8JdjemtYGqdcZK8fs50BTZENXqddJM1GZVmCOqS2wlVM7MIZeo18Zt/s1600/130g.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "<img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiMKumkHV9XPpKSy33kXI1z23kgq-smiWbN5JBPip4v7x2TKRr4UCcqwfHH-LiFXMzVM2RRWlV48JKftz-adTiMOBc-jub8Dh6OvJzXGR52BRpPBxbH83qntdY8vxzVkovuOBqyYdP5Z5ixQZ_zokr1Uqg_v0bUNDiYQmGTzfNtasjq-9BWFjoPlVv8G1vz/s320/127g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 0",
+      "2. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhafgVexCAE6yHUJFM_Te-a9DR1PCSrWHRvfJeN1cL1Gfi2cAffhRl9k88ui4yXTHQx4UcFTRMHcjO2mhNRyprLYVFQHbaS3j2NVyy9vyQZmaasNgvCQAigGIYKxwHGuMaEhzPIbHKziidz53corfccvGeBaKQBmWPQXUEXK27uYQIbggBEH0zOhxP_w1ZK/s1600/128g.png\" style=\"max-width:100%;height:auto;\">",
+      "3. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjbXav5QMOfsbCnB1vs1K6nPjZzM4nBidPu0ogk9iF0Fui9Z0SBycrnWFH2nUGv4xKfBxOI-iN70x6tTbt97YLnBNi_KyATfNH8tZzbk1f3vDwyGrit3mCh-k-W3n2T8Taqz-Q1ZAE3A8CPSlN-uq-h67bDENU-WGXVwGAhTcjo8cU1tZkHiBHFOabyLXiR/s1600/129g.png\" style=\"max-width:100%;height:auto;\">",
+      "4. <img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgmn40NVkbAd-ENbta2-KLtX0z6JLWhEHNPEOHjsOIsZoiKpRdwqtQpC0QcecivbRu7sJSiGpZzf6PhxT_0N-D9X7t3-GT_qMZ2YioU-RN9vHns0Rtyarmc_IMCgKSd_8J93ajXMf8JdjemtYGqdcZK8fs50BTZENXqddJM1GZVmCOqS2wlVM7MIZeo18Zt/s1600/130g.png\" style=\"max-width:100%;height:auto;\">"
+    ],
+    "answer": 1
+  },
+  {
+    "q": "<img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjE_Ahvywf8HUiHNkY3AQNeZGbeX6Mdl4XPqaVecT0viqTiMjgpLIa5B2K5BXFir6yRXfqbhLt_caeBNX2Vs3ZBcXYOuH0bO2Zfnlum7_ZCOKcLbq1vcNsu_Uj9qyyT5HE3CPRfAjUhqLC-w1g7yNSH4BOp7keUJkHURkxhRtblxJRfODOHE87frGMhr-xb/s320/131g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 1",
+      "2. 2",
+      "3. 3",
+      "4. 0"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "<img src=\"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhmrRWqpA92dschrTgZPz22Md5je6vCuFJuDwZ6KkHP6vFEkFR9dm8QuMYcX3KpjSMX2ZoZggixVTbYSw0eJpKjhrx-u6gv8ebIbUqKNS_ChT7fKhfWVyGc4z6vdqSHGFULbmTJgsZru2m1rtHJMUW0cMuGjPCIsbZochvkzmga2198WOZZBE5efEFp6j2X/s320/132g.png\" style=\"max-width:100%;height:auto;\">",
+    "options": [
+      "1. 1",
+      "2. 2",
+      "3. 3",
+      "4. 0"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "The side of a square is 10 cm. A second square is formed by joining mid points of the sides of the given square. Again joining the mid points of the sides of the second square, a third square is formed. This process is continued endlessly. What is the sum of the areas of the infinite sequence of squares?",
+    "options": [
+      "1. 160 cm²",
+      "2. 180 cm²",
+      "3. 200 cm²",
+      "4. 240 cm²"
+    ],
+    "answer": 2
+  },
+  {
+    "q": "एक वर्ग की भुजा 10 cm है। दिए गए वर्ग की भुजाओं के मध्‍य बिंदुओं को मिलाकर एक दूसरा वर्ग बनाया जाता है। पुनः दूसरे वर्ग की भुजाओं के मध्‍य बिन्दुओं को मिलाकर एक तीसरा वर्ग बनाया जाता है। यह प्रक्रिया अंतहीन रूप से जारी रखी जाती है। वर्गों के अनंत अनुक्रम के क्षेत्रफलों का योग क्या है?",
+    "options": [
+      "1. 160 cm²",
+      "2. 180 cm²",
+      "3. 200 cm²",
+      "4. 240 cm²"
+    ],
+    "answer": 2
+  }
+]
